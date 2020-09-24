6208eabfb7617929e2ff0e9da4c5c8fa0a22d7a7
lihereshma <reshmalihe00068@gmail.com>
2020-09-24 14:32:04 +0530

    tabs section functions added

--- assets/js/script.js
+++ assets/js/script.js
@@ -7,3 +7,32 @@ $(document).ready (function() {
   arrows: false
   });  
 });
+
+// Tabs section filtering
+var tabsName = document.querySelectorAll(".tab a");
+var content = document.querySelectorAll(".tab-content li");
+
+var tabArray = Array.from(tabsName);
+tabArray.forEach(function(element){
+  element.addEventListener('click',function(e){
+    var text = e.target.text;
+    var currentTab = this;
+    console.log(text)
+    tabsName.forEach(function(item) {        
+      if (item == currentTab) {
+        item.parentElement.classList.add("active-tab");
+      } 
+      else {
+        item.parentElement.classList.remove("active-tab");
+      }
+    });
+    content.forEach(function(item) {
+      var attr = item.getAttribute("data-attribute");        
+      if (text == attr) {
+        item.classList.add("active-content");
+      } else {
+        item.classList.remove("active-content");
+      }
+    });
+  });
+});
